bab1f30ea63a5a9d7b899cb8eea536810a11a431
Daria Popova <dassenka@yandex.ru>
2023-09-05 23:23:54 +0300

    Sprint 18

---
+++ app/src/main/java/com/practicum/playlistmaker/lib/ui/FavoriteTracksFragment.kt
@@ -0,0 +1,31 @@
+package com.practicum.playlistmaker.lib.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.practicum.playlistmaker.databinding.FragmentFavoriteTracksBinding
+import org.koin.androidx.viewmodel.ext.android.viewModel
+
+class FavoriteTracksFragment : Fragment() {
+
+    private val FavoriteTracksViewModel: FavoriteTracksViewModel by viewModel()
+    private lateinit var binding: FragmentFavoriteTracksBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentFavoriteTracksBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    companion object {
+        fun newInstance() = FavoriteTracksFragment()
+    }
+}
---
+++ app/src/main/java/com/practicum/playlistmaker/lib/ui/PlaylistsFragment.kt
@@ -0,0 +1,31 @@
+package com.practicum.playlistmaker.lib.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.practicum.playlistmaker.databinding.FragmentPlaylistsBinding
+import org.koin.androidx.viewmodel.ext.android.viewModel
+
+class PlaylistsFragment : Fragment(){
+
+    private val PlaylistsViewModel: PlaylistsViewModel by viewModel()
+    private lateinit var binding: FragmentPlaylistsBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentPlaylistsBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    companion object {
+        fun newInstance() = PlaylistsFragment()
+    }
+}
